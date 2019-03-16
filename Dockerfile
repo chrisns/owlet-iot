f101abf1d648f687dd002317c1659cfd2f2332d1
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-03-16 23:33:28 +0000

    subscribe to events

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:alpine
 WORKDIR /app
-COPY package.json  package-lock.json ./
+COPY package.json ./
 RUN npm install && npm audit fix
 COPY index.js .
 USER node
--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const Iot = require("@chrisns/iot-shorthand")
 const iot = new Iot()
 const connect = require('@williamshupe/owlet').connect
 
-const { OWLET_USERNAME, OWLET_PASSWORD } = process.env
+const { OWLET_USERNAME, OWLET_PASSWORD, INTERVAL = 30 } = process.env;
 
-async function run() {
-  const owlet = await connect(OWLET_USERNAME, OWLET_PASSWORD);
+(async () => {
+  const owlet = await connect(OWLET_USERNAME, OWLET_PASSWORD)
 
   const devices = await owlet.getDevices()
-  devices.forEach(device =>
+
+  const event_handler = device_id => payload => {
+    if (payload.isBaseStationOn === true)
+      owlet.turnBaseStationOn(device_id)
+    else if (payload.isBaseStationOn === false)
+      owlet.turnBaseStationOff(device_id)
+  }
+
+  await devices.forEach(async device =>
     iot.discovered({
       name: `owlet_${device.id}`,
       type: "owlet",
@@ -17,10 +25,10 @@ async function run() {
         model: device.model,
         type: device.type
       }
-    })
+    }, event_handler(device.id))
   )
 
-  devices.forEach(device => {
+  devices.forEach(device =>
     setInterval(() =>
       owlet.getProperties(device.id)
         .then(props => {
@@ -28,14 +36,8 @@ async function run() {
             props.oxygenLevel = null
             props.heartRate = null
           }
-          iot.report(`owlet_${device.id}`, props)
-        })
-      , 30 * 1000)
-  })
-  // console.log(devices[0])
-  // owlet.getProperties(deviceId)
-  // .then(response => console.log(deviceId, response))
-  // await owlet.turnBaseStationOn(deviceId);
-  // await owlet.turnBaseStationOff(deviceId);
-};
-run()
+          return iot.report(`owlet_${device.id}`, props)
+        }),
+      INTERVAL * 1000)
+  )
+})()
